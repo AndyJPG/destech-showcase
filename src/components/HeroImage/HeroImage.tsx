04ea39601bc42d35f1d872d9be3a5368d26a3790
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2021-08-27 22:31:14 +1000

    implemented carosul for hero section. Need to work on responsiveness

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -1,22 +1,32 @@
 import './heroImage.scss';
 import * as React from 'react';
+import { Carousel } from "react-bootstrap";
 
 function HeroImage() {
 
     return (
         <div className="hero-container container-fluid">
-            <div className="container-lg">
-                <div className="row align-items-center">
-                    <div className="hero-header-container col-sm-12 col-lg-7">
-                        <div className="style-border">
-                            <h1 className="hero-header">the international conference on design & technology</h1>
-                            <p className="hero-subtitle">Inclusion, Innovation & Interdisciplinary Design</p>
-                        </div>
-                    </div>
-                    <div className="hero-image-container col-sm-12 col-lg-5">
-                        <img className="hero-image" src="/assets/img/hero-image-4.jpg" alt="hero"/>
+            <div className="hero-content-container container-lg">
+                <div className="hero-header-container">
+                    <div className="style-border">
+                        <h1 className="hero-header">the international conference on design & technology</h1>
+                        <p className="hero-subtitle">Inclusion, Innovation & Interdisciplinary Design</p>
                     </div>
                 </div>
+                <Carousel fade className="hero-carousel">
+                    <Carousel.Item>
+                        <img className="d-block w-100" src="assets/img/products/sport-watch.png" alt="sport watch" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src="assets/img/products/drone-image.png" alt="drone" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src="assets/img/products/pen-image.png" alt="pen" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src="assets/img/products/chair-image.png" alt="chair" />
+                    </Carousel.Item>
+                </Carousel>
             </div>
         </div>
     )
