ea5db0166fa349f21bbd9d9862b2fa7bc31d2a21
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2021-08-28 12:41:17 +1000

    implemented carousel on hero image

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -2,6 +2,19 @@ import './heroImage.scss';
 import * as React from 'react';
 import { Carousel } from "react-bootstrap";
 
+type Image = {
+    src: string,
+    alt: string
+}
+
+// Images array
+const images: Array<Image> = [
+    {src: "assets/img/products/sport-watch.png", alt: "sport watch"},
+    {src: "assets/img/products/drone-image.png", alt: "drone"},
+    {src: "assets/img/products/pen-image.png", alt: "pen"},
+    {src: "assets/img/products/chair-image.png", alt: "chair"}
+];
+
 function HeroImage() {
 
     return (
@@ -13,19 +26,12 @@ function HeroImage() {
                         <p className="hero-subtitle">Inclusion, Innovation & Interdisciplinary Design</p>
                     </div>
                 </div>
-                <Carousel fade className="hero-carousel">
-                    <Carousel.Item>
-                        <img className="d-block w-100" src="assets/img/products/sport-watch.png" alt="sport watch" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="d-block w-100" src="assets/img/products/drone-image.png" alt="drone" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="d-block w-100" src="assets/img/products/pen-image.png" alt="pen" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="d-block w-100" src="assets/img/products/chair-image.png" alt="chair" />
-                    </Carousel.Item>
+                <Carousel fade className="hero-carousel" controls={false} variant="dark">
+                    { images.map((image) =>
+                        <Carousel.Item key={image.alt}>
+                            <img className="d-block w-100" src={image.src} alt={image.alt} />
+                        </Carousel.Item>
+                    )}
                 </Carousel>
             </div>
         </div>
