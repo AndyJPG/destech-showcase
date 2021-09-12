7c460502693474c7b23767dac068d45fac93e76e
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2021-09-12 11:55:26 +1000

    finish implementing footer ui

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,12 @@ function Footer() {
         <div className="container-fluid footer">
             <p>Schools of engineering & communication and creative arts deakin university, geelong, VIC +61 3 5247 9360 destech@deakin.edu.au</p>
             <div className="container-lg logo-container">
-                <img src="assets/img/hero-image.jpg" alt="destech logo" />
-                <img src="assets/img/hero-image.jpg" alt="destech logo" />
+                <a href="https://destech.jiangpeigeng.com/">
+                    <img src="assets/img/logo/destech-logo-round.svg" alt="destech logo" />
+                </a>
+                <a href="https://www.deakin.edu.au/">
+                    <img src="assets/img/logo/deakin-logo-round.svg" alt="destech logo" />
+                </a>
             </div>
             <p>&copy; Deakin University CRICOS Provider Code : 00113B</p>
         </div>
